00174251b29cb50b078aa102b409ae371c96a086
Jotas <geunesk@gmail.com>
2020-03-12 20:09:55 +0800

    Added views to create/edit product in the site

--- webkaproject/marketplace1/views.py
+++ webkaproject/marketplace1/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from .models import Comment
+from .forms import ProductForm
+from django.shortcuts import redirect
 from django.utils import timezone
 
 
@@ -12,3 +13,32 @@ def product_list(request):
 def product_detail(request, pk):
     product = get_object_or_404(Post, pk=pk)
     return render(request, 'marketplace/product_detail.html', {'post': product})
+
+
+def product_new(request):
+    if(request.POST):
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('product_detail',pk = post.pk )
+    else:
+        form = ProductForm()
+        return render(request, 'marketplace/product_new.html', {'form': form})
+
+def product_edit(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = ProductForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('product_detail', pk=post.pk)
+
+    else:
+        form = ProductForm(instance=post)
+    return render(request, 'marketplace/product_edit.html', {'form': form})
