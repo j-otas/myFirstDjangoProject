d5e1d6e2534c125cd8fedbf19d2c2c4137a356e5
Jotas <geunesk@mail.ru>
2021-11-27 20:19:01 +0800

    ADD: Редактирование элементов таблиц работает полностью

--- webkaproject/admin_panel/forms.py
+++ webkaproject/admin_panel/forms.py
@@ -1,12 +1,3 @@
 from django import forms
 from bootstrap_modal_forms.generic import BSModalCreateView
 import models
-
-class objectForm(forms.ModelForm, selected_model):
-    class Meta:
-        model = selected_model
-
-    template_name = 'templates/admin_table.html'
-    form_class = models.User
-    success_message = 'Success: Book was created.'
-    success_url = reverse_lazy('index')
--- webkaproject/admin_panel/views.py
+++ webkaproject/admin_panel/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.apps import apps
 from django.shortcuts import get_object_or_404
 from django.forms import modelform_factory,inlineformset_factory
-
+from django.forms.models import fields_for_model
 class Counter:
     count = 0
 
@@ -17,7 +17,8 @@ class Counter:
     def double(self):
         self.count *= 2
         return ''
-
+from django.forms import ModelForm
+from django.forms.models import model_to_dict
 def get_model(num):
     mods = apps.get_models()  # все модели
     model_names = []
@@ -28,7 +29,7 @@ def get_model(num):
             models.append(mod)
     return models[num]
 
-def get_values_of_object(objects, object_fields ):
+def get_values_of_objects(objects, object_fields ):
     values = []
     for obj in objects:
         print(obj)
@@ -37,6 +38,8 @@ def get_values_of_object(objects, object_fields ):
             temp.append(getattr(obj, field.name))
         values.append(temp)
     return values
+
+
 
 def main_admin_panel(request):
     mods = apps.get_models()  # все модели
@@ -49,6 +52,7 @@ def main_admin_panel(request):
 
     return render(request, 'admin_panel/main_admin_list_page.html', {'table_names': model_names, 'counter': counter})
 
+
 def admin_current_table(request, pk):
 
     selected_model = get_model(pk) # выбранная пользователем модель(тип данных)
@@ -57,7 +61,7 @@ def admin_current_table(request, pk):
 
     selected_model_objects = selected_model.objects.order_by('id')# все объекты выбранного типа данных отсортированные по id
 
-    values_of_fields = get_values_of_object(selected_model_objects, selected_model_fields) #значения объекта
+    values_of_fields = get_values_of_objects(selected_model_objects, selected_model_fields) #значения объекта
 
     return render(request, 'admin_panel/admin_table.html', {'selected_model_name': selected_model._meta.verbose_name,
                                                             'selected_model_objects': selected_model_objects,
@@ -69,24 +73,22 @@ def admin_current_table(request, pk):
 def edit_table(request, pk, idd):
     selected_model = get_model(pk)  # выбранная пользователем модель(тип данных)
     selected_model_fields = selected_model._meta.fields  # поля принадлежащие модели
-
-    temp = get_object_or_404(selected_model, pk=idd) # получаем объект выбранной модели
-    obj = []
-    for field in selected_model_fields:
-        obj.append(getattr(temp, field.name))
-
-
-    values_of_fields = []
-
-    field_names = []
-    for field in selected_model_fields:
+    field_names = [] #имена полей(столбцов таблицы)
+    obj = get_object_or_404(selected_model, pk=idd) # получаем объект выбранной модели
+    for field in selected_model_fields: # получаем список имён полей для генерации формы
         if field.editable:
             field_names.append(field.name)
-    form = modelform_factory(selected_model, fields = field_names)
-    print(form)
+
+    temp_form = modelform_factory(selected_model, fields=field_names)#Форма для выбранного объекта, может используется для создания новой формы, поэтому temp
+    form = temp_form(instance=obj)
+    if request.method == "POST":
+        form = temp_form(request.POST, instance=obj)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.save()
+
     return render(request, 'admin_panel/edit_table.html', {'selected_model_name': selected_model._meta.verbose_name,
                                                            'fields': selected_model_fields,
-                                                           'values_of_fields': values_of_fields,
-                                                           'obj': obj,
                                                            'tabnum': int(pk),
-                                                           'form': form}, )
+                                                           'form': form,
+                                                           'obj': obj}, )
